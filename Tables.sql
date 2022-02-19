c67862ba0e6618ead1043297c4b517b514850597
Mano chitra Kumar <95401250+Manochitra20@users.noreply.github.com>
2022-02-18 21:46:16 -0800

    Tables and analysis

--- Tables.sql
+++ Tables.sql
@@ -2,13 +2,15 @@
 DROP TABLE departments;
 --creating table departments:
 CREATE TABLE departments (
-  dept_no VARCHAR Primary key,
+  dept_no VARCHAR(30) Primary key,
   dept_name VARCHAR(30) NOT NULL
 );
 
 SELECT * FROM departments;
 
 --creating table titles
+DROP TABLE titles;
+
 CREATE TABLE titles(
 	title_id VARCHAR(10) PRIMARY KEY,
 	title VARCHAR(50) NOT NULL
@@ -34,15 +36,19 @@ CREATE TABLE employees(
 SELECT * FROM employees;
 
 --creating table dept_emp
+DROP TABLE dept_emp;
+
 CREATE TABLE dept_emp(
-  emp_no INTEGER NOT NULL,
+  emp_no INTEGER,
   dept_no VARCHAR(30) NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
   FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
 SELECT * FROM dept_emp;
 
 --creating table dept_managers
+DROP TABLE dept_managers;
 CREATE TABLE dept_managers(
 	dept_no VARCHAR(10) NOT NULL,
 	emp_no INTEGER NOT NULL,
@@ -53,9 +59,10 @@ CREATE TABLE dept_managers(
 SELECT * FROM dept_managers;
 
 --creating table salaries
+DROP TABLE salaries
 CREATE TABLE salaries(
 	emp_no INTEGER NOT NULL,
-	salary BIGINT NOT NULL,
+	salary INTEGER NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
